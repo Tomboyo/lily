b65afdb0f89ab7852633bde66323ce7863ebad6f
Tom Simmons <thsimmon@redhat.com>
2021-12-11 15:50:45 -0500

    Test in-line object serialization

--- modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/SerializationTest.java
+++ modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/SerializationTest.java
@@ -1,7 +1,8 @@
 package com.github.tomboyo.lily.itproject;
 
 import com.example.MyObject;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.example.MyObject2;
+import com.example.myobject2.Foo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Nested;
@@ -13,7 +14,7 @@ import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.tomboyo.lily.itproject.testsupport.Support.assertJsonEquals;
 
 public class SerializationTest {
   private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -29,28 +30,7 @@ public class SerializationTest {
   public class MyObjectTests {
     @Test
     public void toJson() throws Exception {
-      var obj = new MyObject(
-        true,
-          BigInteger.ZERO,
-          1,
-          2L,
-          BigDecimal.valueOf(3L),
-          4.1d,
-          5.2f,
-          "foo",
-          "password",
-          new Byte[]{ (byte) 3},
-          new Byte[]{ (byte) 7},
-          LocalDate.of(2021, 1, 1),
-          ZonedDateTime.parse("2021-01-01T00:00:00.012Z")
-      );
-
-      var actual =
-          MAPPER.readValue(
-              MAPPER.writeValueAsString(obj),
-              JsonNode.class);
-
-      var expected = MAPPER.readValue(
+      assertJsonEquals(
           """
               {
                 "a": true,
@@ -68,9 +48,39 @@ public class SerializationTest {
                 "m": "2021-01-01T00:00:00.012Z"
               }
               """,
-          JsonNode.class);
-
-      assertEquals(expected, actual);
+          new MyObject(
+              true,
+              BigInteger.ZERO,
+              1,
+              2L,
+              BigDecimal.valueOf(3L),
+              4.1d,
+              5.2f,
+              "foo",
+              "password",
+              new Byte[]{ (byte) 3},
+              new Byte[]{ (byte) 7},
+              LocalDate.of(2021, 1, 1),
+              ZonedDateTime.parse("2021-01-01T00:00:00.012Z")
+          ),
+          MAPPER
+      );
     }
+  }
+
+  @Nested
+  public class MyObject2Tests {
+    @Test
+    public void toJson() throws Exception {
+      assertJsonEquals("""
+            {
+              "foo": {
+                "bar": "value"
+              }
+            }""",
+          new MyObject2(
+              new Foo("value")),
+          MAPPER);
+      }
   }
 }
