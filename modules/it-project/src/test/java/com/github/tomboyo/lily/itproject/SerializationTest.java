595267c84d28806da6c5cddace0fdc376123f7f7
Tom Simmons <thsimmon@redhat.com>
2021-12-11 22:01:17 -0500

    Parameterize serialization tests

--- modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/SerializationTest.java
+++ modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/SerializationTest.java
@@ -4,22 +4,29 @@ import com.example.MyObject;
 import com.example.MyObject2;
 import com.example.MySimpleAlias;
 import com.example.myobject2.Foo;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
-import static com.github.tomboyo.lily.itproject.testsupport.Support.assertJsonEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+/** Tests that all generated sources serialize and deserialize to expected values. */
 public class SerializationTest {
   private static final ObjectMapper MAPPER = new ObjectMapper();
 
@@ -30,12 +37,36 @@ public class SerializationTest {
         .configure(WRITE_DATES_AS_TIMESTAMPS, false);
   }
 
-  @Nested
-  public class MyObjectTests {
-    @Test
-    public void toJson() throws Exception {
-      assertJsonEquals(
-          """
+  @ParameterizedTest
+  @MethodSource("parameterSource")
+  public void toJson(TestParameter<?> params) throws Exception {
+    var actualJson = MAPPER.readValue(MAPPER.writeValueAsString(params.asObject), JsonNode.class);
+    var expectedJson = MAPPER.readValue(params.asJson, JsonNode.class);
+    assertEquals(expectedJson, actualJson);
+  }
+
+  @ParameterizedTest
+  @MethodSource("parameterSource")
+  public <T> void fromJson(TestParameter<T> params) throws Exception {
+    params.assertObjectEquals.accept(
+        params.asObject,
+        MAPPER.readValue(params.asJson, params.type())
+    );
+  }
+
+  private record TestParameter<T>(String asJson, T asObject, Class<T> type, BiConsumer<T, T> assertObjectEquals) {}
+
+  public static Stream<Arguments> parameterSource() {
+    return Stream.of(
+        myObjectTestParameter(),
+        myObject2TestParameter(),
+        mySimpleAliasTestParameter())
+        .map(x -> arguments(Named.of(x.type.getSimpleName(), x)));
+  }
+
+  private static TestParameter<MyObject> myObjectTestParameter() {
+    return new TestParameter<>(
+        """
               {
                 "a": true,
                 "b": 0,
@@ -52,113 +83,45 @@ public class SerializationTest {
                 "m": "2021-01-01T00:00:00.012Z"
               }
               """,
-          new MyObject(
-              true,
-              BigInteger.ZERO,
-              1,
-              2L,
-              BigDecimal.valueOf(3L),
-              4.1d,
-              5.2f,
-              "foo",
-              "password",
-              new Byte[]{ (byte) 3},
-              new Byte[]{ (byte) 7},
-              LocalDate.of(2021, 1, 1),
-              OffsetDateTime.parse("2021-01-01T00:00:00.012Z")
-          ),
-          MAPPER
-      );
-    }
-
-    @Test
-    public void fromJson() throws Exception {
-      var actual = MAPPER.readValue("""
-              {
-                "a": true,
-                "b": 0,
-                "c": 1,
-                "d": 2,
-                "e": 3,
-                "f": 4.1,
-                "g": 5.2,
-                "h": "foo",
-                "i": "password",
-                "j": [3],
-                "k": [7],
-                "l": "2021-01-01",
-                "m": "2021-01-01T00:00:00.012Z"
-              }
-              """, MyObject.class);
-      var expected = new MyObject(
-          true,
-          BigInteger.ZERO,
-          1,
-          2L,
-          BigDecimal.valueOf(3L),
-          4.1d,
-          5.2f,
-          "foo",
-          "password",
-          new Byte[]{ (byte) 3},
-          new Byte[]{ (byte) 7},
-          LocalDate.of(2021, 1, 1),
-          OffsetDateTime.parse("2021-01-01T00:00:00.012Z")
-      );
-
-      // Byte[]#equals() is by object equality, so we must decompose our assertEquals.
-      assertEquals(
-          List.of(expected.a(), expected.b(), expected.c(), expected.d(), expected.e(), expected.f(), expected.g(), expected.h(), expected.i(), expected.l(), expected.m()),
-          List.of(actual.a(), actual.b(), actual.c(), actual.d(), actual.e(), actual.f(), actual.g(), actual.h(), actual.i(), actual.l(), actual.m()));
-      assertArrayEquals(expected.j(), actual.j());
-      assertArrayEquals(expected.k(), actual.k());
-    }
+        new MyObject(true,
+            BigInteger.ZERO,
+            1,
+            2L,
+            BigDecimal.valueOf(3L),
+            4.1d,
+            5.2f,
+            "foo",
+            "password",
+            new Byte[]{(byte) 3},
+            new Byte[]{(byte) 7},
+            LocalDate.of(2021, 1, 1),
+            OffsetDateTime.parse("2021-01" +
+                "-01T00:00:00.012Z")),
+        MyObject.class,
+        (expected, actual) -> {
+          assertEquals(
+              List.of(expected.a(), expected.b(), expected.c(), expected.d(), expected.e(), expected.f(),
+                  expected.g(), expected.h(), expected.i(), expected.l(), expected.m()),
+              List.of(actual.a(), actual.b(), actual.c(), actual.d(), actual.e(), actual.f(), actual.g(),
+                  actual.h(), actual.i(), actual.l(), actual.m()));
+          assertArrayEquals(expected.j(), actual.j());
+          assertArrayEquals(expected.k(), actual.k());
+        });
   }
 
-  @Nested
-  public class MyObject2Tests {
-    @Test
-    public void toJson() throws Exception {
-      assertJsonEquals("""
-            {
-              "foo": {
-                "bar": "value"
-              }
-            }""",
-          new MyObject2(
-              new Foo("value")),
-          MAPPER);
-      }
-
-    @Test
-    public void fromJson() throws Exception {
-      assertEquals(
-          new MyObject2(new Foo("value")),
-          MAPPER.readValue("""
-            {
-              "foo": {
-                "bar": "value"
-              }
-            }""", MyObject2.class));
-    }
+  private static TestParameter<MyObject2> myObject2TestParameter() {
+    return new TestParameter<>(
+        "{ \"foo\": { \"bar\": \"value\" } }",
+        new MyObject2(new Foo("value")),
+        MyObject2.class,
+        Assertions::assertEquals);
   }
 
-  @Nested
-  public class MySimpleAliasTests {
-    @Test
-    public void toJson() throws Exception {
-      assertJsonEquals(
-          "\"value-string\"",
-          new MySimpleAlias("value-string"),
-          MAPPER);
-    }
-
-    @Test
-    public void fromJson() throws Exception {
-      assertEquals(
-          MAPPER.readValue("\"value-string\"", MySimpleAlias.class),
-          new MySimpleAlias("value-string")
-      );
-    }
+  private static TestParameter<MySimpleAlias> mySimpleAliasTestParameter() {
+    return new TestParameter<>(
+        "\"value\"",
+        new MySimpleAlias("value"),
+        MySimpleAlias.class,
+        Assertions::assertEquals);
   }
 }
