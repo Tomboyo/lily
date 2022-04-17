48d824976dc1361cccbbe09e68db8ed2313a735f
Tom Simmons <thsimmon@redhat.com>
2022-04-17 13:17:56 -0400

    Move request and response example to it-project

---
+++ modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/WiremockSupport.java
@@ -0,0 +1,17 @@
+package com.github.tomboyo.lily.itproject;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+public class WiremockSupport {
+  private WiremockSupport() {}
+
+  public static HttpRequest.Builder newBuilder(WireMockRuntimeInfo info) {
+    return newBuilder(info, "");
+  }
+
+  public static HttpRequest.Builder newBuilder(WireMockRuntimeInfo info, String path) {
+    return HttpRequest.newBuilder(URI.create(info.getHttpBaseUrl() + "/" + path));
+  }
+}
