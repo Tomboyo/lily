dc6fa85adfd6528afb9fdd250df4aef44f88ffd4
Tom Simmons <thsimmon@redhat.com>
2022-05-08 11:52:45 -0400

    Update tests

--- modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/ParametersTest.java
+++ modules/it-project/src/test/java/com/github/tomboyo/lily/itproject/ParametersTest.java
@@ -1,18 +1,18 @@
 package com.github.tomboyo.lily.itproject;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.anyRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomboyo.lily.http.encoding.Encoding.simple;
 import static com.github.tomboyo.lily.itproject.WiremockSupport.newBuilder;
 import static java.net.http.HttpResponse.BodyHandlers.discarding;
 
 import com.example.RGB;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import com.github.tomboyo.lily.http.encoding.Encoding;
 import java.net.http.HttpClient;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Nested;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 @WireMockTest
@@ -20,42 +20,17 @@ public class ParametersTest {
 
   private final HttpClient client = HttpClient.newBuilder().build();
 
-  // TODO: inline parameter schema generation is not supported.
-  @Nested
-  public class SchemaGeneration {
-    @Test
-    @Disabled
-    public void pathInline() {}
+  /** See oas.yaml paths.simpleParameterStyle */
+  @Test
+  public void simpleParameterStyle(WireMockRuntimeInfo info) throws Exception {
+    client.send(
+        newBuilder(info, "/simpleParameterStyle/" + simple(new RGB(100, 200, 255)))
+            .header("MyHeader", simple(List.of(1, 2, 3)))
+            .build(),
+        discarding());
 
-    @Test
-    @Disabled
-    public void queryInline() {}
-
-    @Test
-    @Disabled
-    public void headerInline() {}
-
-    @Test
-    @Disabled
-    public void cookieInline() {}
-  }
-
-  @Nested
-  public class InPath {
-
-    @Nested
-    public class Simple {
-
-      @Test
-      public void object(WireMockRuntimeInfo info) throws Exception {
-        client.send(
-            newBuilder(
-                    info, "/parametersSimpleObjectRef/" + Encoding.simple(new RGB(100, 200, 255)))
-                .build(),
-            discarding());
-
-        verify(anyRequestedFor(urlPathEqualTo("/parametersSimpleObjectRef/r,100,g,200,b,255")));
-      }
-    }
+    verify(
+        anyRequestedFor(urlPathEqualTo("/simpleParameterStyle/r,100,g,200,b,255"))
+            .withHeader("MyHeader", equalTo("1,2,3")));
   }
 }
