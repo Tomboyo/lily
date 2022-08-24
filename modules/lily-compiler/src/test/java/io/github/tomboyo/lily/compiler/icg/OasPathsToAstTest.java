eebabacbbd5e0de069d11d23be4ce996aa7c40e9
Tom Simmons <thsimmon@redhat.com>
2022-08-23 20:42:49 -0400

    organize tests into nested class

--- modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/icg/OasPathsToAstTest.java
+++ modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/icg/OasPathsToAstTest.java
@@ -13,70 +13,77 @@ import io.swagger.v3.oas.models.media.IntegerSchema;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class OasPathsToAstTest {
 
-  @Test
-  void generatesAstForAllParameters() {
-    try (var mock = mockStatic(OasSchemaToAst.class)) {
-      mock.when(() -> OasSchemaToAst.evaluate(any(), any(), any()))
-          .thenAnswer(invocation -> new Pair<>(astBoolean(), Stream.of()));
+  @Nested
+  class Parameters {
+    @Test
+    void areEvaluatedToAst() {
+      try (var mock = mockStatic(OasSchemaToAst.class)) {
+        mock.when(() -> OasSchemaToAst.evaluate(any(), any(), any()))
+            .thenAnswer(invocation -> new Pair<>(astBoolean(), Stream.of()));
 
-      OasPathsToAst.evaluatePathItem(
-              "p",
-              new PathItem()
-                  .addParametersItem(
-                      new Parameter().name("a").in("path").schema(new BooleanSchema()))
-                  .addParametersItem(
-                      new Parameter().name("b").in("path").schema(new StringSchema()))
-                  .get(
-                      new Operation()
-                          .operationId("Get")
-                          .addParametersItem(
-                              new Parameter().name("c").in("query").schema(new IntegerSchema()))
-                          .addParametersItem(
-                              new Parameter().name("d").in("header").schema(new BooleanSchema()))))
-          .forEach(
-              x -> {
-                ;
-              }); // consume the stream.
+        OasPathsToAst.evaluatePathItem(
+                "p",
+                new PathItem()
+                    .addParametersItem(
+                        new Parameter().name("a").in("path").schema(new BooleanSchema()))
+                    .addParametersItem(
+                        new Parameter().name("b").in("path").schema(new StringSchema()))
+                    .get(
+                        new Operation()
+                            .operationId("Get")
+                            .addParametersItem(
+                                new Parameter().name("c").in("query").schema(new IntegerSchema()))
+                            .addParametersItem(
+                                new Parameter()
+                                    .name("d")
+                                    .in("header")
+                                    .schema(new BooleanSchema()))))
+            .forEach(
+                x -> {
+                  ;
+                }); // consume the stream.
 
-      // Schema are generated for all parameters according to OasSchemaToAst.
-      mock.verify(
-          () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("A"), eq(new BooleanSchema())));
-      mock.verify(
-          () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("B"), eq(new StringSchema())));
-      mock.verify(
-          () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("C"), eq(new IntegerSchema())));
-      mock.verify(
-          () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("D"), eq(new BooleanSchema())));
+        // Schema are generated for all parameters according to OasSchemaToAst.
+        mock.verify(
+            () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("A"), eq(new BooleanSchema())));
+        mock.verify(
+            () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("B"), eq(new StringSchema())));
+        mock.verify(
+            () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("C"), eq(new IntegerSchema())));
+        mock.verify(
+            () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("D"), eq(new BooleanSchema())));
+      }
     }
-  }
 
-  @Test
-  void operationParametersOverrideInheritedParameters() {
-    try (var mock = mockStatic(OasSchemaToAst.class)) {
-      mock.when(() -> OasSchemaToAst.evaluate(any(), any(), any()))
-          .thenAnswer(invocation -> new Pair<>(astBoolean(), Stream.of()));
+    @Test
+    void operationParametersOverridePathItemParameters() {
+      try (var mock = mockStatic(OasSchemaToAst.class)) {
+        mock.when(() -> OasSchemaToAst.evaluate(any(), any(), any()))
+            .thenAnswer(invocation -> new Pair<>(astBoolean(), Stream.of()));
 
-      OasPathsToAst.evaluatePathItem(
-              "p",
-              new PathItem()
-                  .addParametersItem(
-                      new Parameter().name("a").in("query").schema(new BooleanSchema()))
-                  .get(
-                      new Operation()
-                          .operationId("Get")
-                          .addParametersItem(
-                              new Parameter().name("a").in("query").schema(new IntegerSchema()))))
-          .forEach(
-              x -> {
-                ; // consume the stream
-              });
+        OasPathsToAst.evaluatePathItem(
+                "p",
+                new PathItem()
+                    .addParametersItem(
+                        new Parameter().name("a").in("query").schema(new BooleanSchema()))
+                    .get(
+                        new Operation()
+                            .operationId("Get")
+                            .addParametersItem(
+                                new Parameter().name("a").in("query").schema(new IntegerSchema()))))
+            .forEach(
+                x -> {
+                  ; // consume the stream
+                });
 
-      mock.verify(
-          () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("A"), eq(new IntegerSchema())));
+        mock.verify(
+            () -> OasSchemaToAst.evaluate(eq("p.getoperation"), eq("A"), eq(new IntegerSchema())));
+      }
     }
   }
 }
