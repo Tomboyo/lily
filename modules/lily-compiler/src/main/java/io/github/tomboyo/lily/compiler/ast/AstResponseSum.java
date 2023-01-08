ffadc46a0d122e052308ae6290da1a5d63850e31
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2023-01-07 22:19:35 -0500

    Add documentation

--- modules/lily-compiler/src/main/java/io/github/tomboyo/lily/compiler/ast/AstResponseSum.java
+++ modules/lily-compiler/src/main/java/io/github/tomboyo/lily/compiler/ast/AstResponseSum.java
@@ -2,4 +2,8 @@ package io.github.tomboyo.lily.compiler.ast;
 
 import java.util.Map;
 
+/**
+ * A sum type over all operation responses. For example, GetFooResponse with members GetFoo200 and
+ * GetFoo404.
+ */
 public record AstResponseSum(Fqn name, Map<String, Fqn> statusCodeToMember) implements Ast {}
