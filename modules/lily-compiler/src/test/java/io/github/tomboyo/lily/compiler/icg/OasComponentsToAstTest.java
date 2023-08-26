541c1cf875c76c5dc45005522666e029299674f0
Tom Simmons <thsimmon@redhat.com>
2023-08-26 13:40:37 -0400

    flesh out ObjectTests

--- modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/icg/OasComponentsToAstTest.java
+++ modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/icg/OasComponentsToAstTest.java
@@ -2,6 +2,7 @@ package io.github.tomboyo.lily.compiler.icg;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,75 +10,14 @@ import io.github.tomboyo.lily.compiler.LilyExtension;
 import io.github.tomboyo.lily.compiler.LilyExtension.LilyTestSupport;
 import io.github.tomboyo.lily.compiler.ast.PackageName;
 import io.github.tomboyo.lily.compiler.ast.SimpleName;
-import io.github.tomboyo.lily.compiler.cg.Mustache;
 import io.swagger.v3.oas.models.media.ObjectSchema;
-import java.util.Map;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class OasComponentsToAstTest {
-
-  @Nested
-  class ScalarComponents {
-
-    @RegisterExtension
-    static LilyExtension extension = LilyExtension.newBuilder().packagePerMethod().build();
-
-    @ParameterizedTest
-    @CsvSource({
-      "boolean, null, java.lang.Boolean.TRUE",
-      "boolean, unsupported-format, java.lang.Boolean.TRUE",
-      "integer, null, java.math.BigInteger.ONE",
-      "integer, unsupported-format, java.math.BigInteger.ONE",
-      "integer, int32, 1", // integer
-      "integer, int64, 1L", // long
-      "number, null, java.math.BigDecimal.ONE",
-      "number, unsupported-format, java.math.BigDecimal.ONE",
-      "number, double, 1d", // double
-      "number, float, 1f", // float
-      "string, null, \"string\"",
-      "string, unsupportedFormat, \"string\"",
-      "string, password, \"string\"",
-      "string, byte, java.nio.ByteBuffer.allocate(1)",
-      "string, binary, java.nio.ByteBuffer.allocate(1)",
-      "string, date, java.time.LocalDate.now()",
-      "string, date-time, java.time.OffsetDateTime.now()"
-    })
-    void test(String oasType, String oasFormat, String value, LilyTestSupport support) {
-      support.compileOas(
-          Mustache.writeString(
-              """
-              openapi: 3.0.2
-              components:
-                schemas:
-                  Foo:
-                    properties:
-                      p:
-                        type: {{type}}
-                        format: {{format}}
-              """,
-              "scalar-components-test",
-              Map.of(
-                  "type", oasType,
-                  "format", oasFormat)));
-
-      assertTrue(
-          support.evaluate(
-              """
-                var value = {{value}};
-                return value == new {{package}}.Foo(value).p();
-                """,
-              Boolean.class,
-              "value",
-              value));
-    }
-  }
 
   @Nested
   @ExtendWith(LilyExtension.class)
@@ -112,7 +52,7 @@ public class OasComponentsToAstTest {
     }
 
     @Test
-    void testJson(LilyTestSupport support) throws JsonProcessingException {
+    void testSer(LilyTestSupport support) throws JsonProcessingException {
       var alias =
           support.evaluate(
               """
@@ -125,6 +65,11 @@ public class OasComponentsToAstTest {
           "{\"foo\":\"foo!\"}",
           mapper.writeValueAsString(alias),
           "Alias are serialized as if they were only their aliased value");
+    }
+
+    @Test
+    void testDeser(LilyTestSupport support) {
+      fail("Todo!");
     }
   }
 
@@ -173,66 +118,6 @@ public class OasComponentsToAstTest {
             "[true,false]",
             mapper.writeValueAsString(right),
             "Arrays types serialize to bare arrays");
-      }
-    }
-
-    @Nested
-    @ExtendWith(LilyExtension.class)
-    class WithImplicitObjectItem {
-
-      @BeforeAll
-      static void beforeAll(LilyTestSupport support) {
-        support.compileOas(
-            """
-                openapi: 3.0.2
-                components:
-                  schemas:
-                    MyComponent:
-                      type: array
-                      items:
-                        properties:
-                          foo:
-                            type: string
-                """);
-      }
-
-      @Test
-      void testValue(LilyTestSupport support) {
-        assertTrue(
-            support.evaluate(
-                """
-                var value = java.util.List.of(new {{package}}.mycomponent.MyComponentItem("foo!"));
-                return value == new {{package}}.MyComponent(value).value();
-                """,
-                Boolean.class));
-      }
-
-      @Test
-      void testSer(LilyTestSupport support) throws JsonProcessingException {
-        var value =
-            support.evaluate(
-                """
-                var value = java.util.List.of(new {{package}}.mycomponent.MyComponentItem("foo!"));
-                return new {{package}}.MyComponent(value);
-                """);
-        var mapper = new ObjectMapper();
-        assertEquals("[{\"foo\":\"foo!\"}]", mapper.writeValueAsString(value));
-      }
-
-      @Test
-      void testDeser(LilyTestSupport support)
-          throws ClassNotFoundException, JsonProcessingException {
-        var expected =
-            support.evaluate(
-                """
-                var value = java.util.List.of(new {{package}}.mycomponent.MyComponentItem("foo!"));
-                return new {{package}}.MyComponent(value);
-                """);
-        var mapper = new ObjectMapper();
-        assertEquals(
-            expected,
-            mapper.readValue(
-                "[{\"foo\":\"foo!\"}]", support.getClassForName("{{package}}.MyComponent")));
       }
     }
 
@@ -302,8 +187,6 @@ public class OasComponentsToAstTest {
       }
     }
 
-    //  TODO: do we need to test implicit/explicit here? I think just recusing once should be enough
-    // right?
     @Nested
     @ExtendWith(LilyExtension.class)
     class WithArrayOfExplicitObjectItem {
