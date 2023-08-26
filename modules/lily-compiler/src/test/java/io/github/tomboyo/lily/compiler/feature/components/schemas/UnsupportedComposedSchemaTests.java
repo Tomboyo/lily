eed1d2aed65e259561782ed1999f948acb8eeb47
Tom Simmons <thsimmon@redhat.com>
2023-08-26 16:35:40 -0400

    Move unsupported composed schema tests

--- modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/feature/components/schemas/UnsupportedComposedSchemaTests.java
+++ modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/feature/components/schemas/UnsupportedComposedSchemaTests.java
@@ -1,21 +1,18 @@
-package io.github.tomboyo.lily.compiler.feature;
+package io.github.tomboyo.lily.compiler.feature.components.schemas;
 
 import io.github.tomboyo.lily.compiler.LilyExtension;
 import io.github.tomboyo.lily.compiler.LilyExtension.LilyTestSupport;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-public class ComponentSchemaTest {
-  @Nested
-  @ExtendWith(LilyExtension.class)
-  class WhenComposedSchema {
-    @BeforeAll
-    static void beforeAll(LilyTestSupport support) {
-      support.compileOas(
-          """
+@ExtendWith(LilyExtension.class)
+public class UnsupportedComposedSchemaTests {
+  @BeforeAll
+  static void beforeAll(LilyTestSupport support) {
+    support.compileOas(
+        """
           openapi: 3.0.2
           components:
             schemas:
@@ -44,34 +41,33 @@ public class ComponentSchemaTest {
                     foo:
                       type: string
               """);
-    }
+  }
 
-    @Test
-    void testThatClassesExist(LilyTestSupport support) {
-      Assertions.assertDoesNotThrow(
-          () ->
-              support.evaluate(
-                  """
+  @Test
+  void testThatClassesExist(LilyTestSupport support) {
+    Assertions.assertDoesNotThrow(
+        () ->
+            support.evaluate(
+                """
                   new {{package}}.MyAllOfSchema();
                   new {{package}}.MyAnyOfSchema();
                   new {{package}}.MyNotSchema();
 
                   return null;
                   """),
-          """
+        """
            If an object schema specification contains compositional keywords allOf, anyOf, or not fields, Lily
            should generate a class so that the code compiles even though it doesn't implement these keywords.
            """);
-    }
+  }
 
-    @Test
-    void testThatJavadocContainsWarning(LilyTestSupport support) {
-      var string = support.getFileStringForClass("{{package}}.MyAllOfSchema");
-      var containsString =
-          string.contains(
-              "Generated empty class because compositional keywords *allOf, anyOf, and not*"
-                  + " are not yet supported");
-      Assertions.assertTrue(containsString, "Lily warns users when it generated empty classes.");
-    }
+  @Test
+  void testThatJavadocContainsWarning(LilyTestSupport support) {
+    var string = support.getFileStringForClass("{{package}}.MyAllOfSchema");
+    var containsString =
+        string.contains(
+            "Generated empty class because compositional keywords *allOf, anyOf, and not*"
+                + " are not yet supported");
+    Assertions.assertTrue(containsString, "Lily warns users when it generated empty classes.");
   }
 }
