bcf1e37b9ebd25d3154a0d1bd8a2823c1008e28a
Tomboyo <tomasimmons@gmail.com>
2023-10-07 20:10:20 -0400

    rewrite tests

--- modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/feature/components/schemas/ComposedTests.java
+++ modules/lily-compiler/src/test/java/io/github/tomboyo/lily/compiler/feature/components/schemas/ComposedTests.java
@@ -14,64 +14,85 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class ComposedTests {
 
   @Nested
-  @ExtendWith(LilyExtension.class)
   class MandatoryProperties {
-    @BeforeAll
-    static void beforeAll(LilyTestSupport support) {
-      support.compileOas(
-          """
+    static final String fragment =
+        """
+        required: ['mandatory1', 'mandatory2', 'optional3']
+        properties:
+          # required and implicitly non-nullable
+          mandatory1:
+            type: string
+          # required and explicitly non-nullable
+          mandatory2:
+            type: string
+            nullable: false
+          # Not required (and implicitly non-nullable)
+          optional1:
+            type: string
+          # Not required (and explicitly non-nullable)
+          optional2:
+            type: string
+            nullable: false
+          # Nullable (and required)
+          optional3:
+            type: string
+            nullable: true
+        """;
+
+    @Nested
+    @ExtendWith(LilyExtension.class)
+    class PropertiesKeyword {
+      @BeforeAll
+      static void beforeAll(LilyTestSupport support) {
+        support.compileOas(
+            """
               openapi: 3.0.3
               components:
                 schemas:
                   Foo:
-                    properties:
-                      a:
-                        type: string
-                        nullable: true
-                      b:
-                        type: string
-                      c:
-                        type: string
-                      d:
-                        type: string
-                        nullable: false
-                    required: ['a', 'c', 'd']
-              """);
-    }
-
-    @ParameterizedTest
-    @CsvSource({"c", "d"})
-    void mandatoryProperties(String name, LilyTestSupport support) {
-      assertTrue(
-          support.evaluate(
+              %s
               """
-              var value = "foo!";
-              return value == {{package}}.Foo.newBuilder()
-                  .set{{Name}}(value)
-                  .build()
-                  .get{{Name}}();
-              """,
-              Boolean.class,
-              "Name",
-              name.toUpperCase()));
-    }
+                .formatted(fragment.indent(6)));
+      }
 
-    @ParameterizedTest
-    @CsvSource({"a", "b"})
-    void nonMandatoryProperties(String name, LilyTestSupport support) {
-      assertTrue(
-          support.evaluate(
-              """
-              var value = "foo!";
-              java.util.Optional<String> actual = {{package}}.Foo.newBuilder()
-                  .set{{Name}}(value)
-                  .build()
-                  .get{{Name}}();
-              return value == actual.orElseThrow();
-              """,
-              Boolean.class,
-              "Name",
-              name.toUpperCase()));
+      @ParameterizedTest
+      @CsvSource({"Mandatory1", "Mandatory2"})
+      void mandatoryProperties(String name, LilyTestSupport support) {
+        assertTrue(
+            support.evaluate(
+                """
+                var foo = "foo!";
+                return {{package}}.Foo.newBuilder()
+                    .set{{Name}}(foo)
+                    .build()
+                    .get{{Name}}() == foo;
+                """,
+                Boolean.class,
+                "Name",
+                name),
+            """
+            Foo's builder must define a setter function, and mandatory properties must has an associated
+            non-Optional typed getter.
+            """);
+      }
+
+      @ParameterizedTest
+      @CsvSource({"Optional1", "Optional2", "Optional3"})
+      void optionalProperties(String name, LilyTestSupport support) {
+        assertTrue(
+            support.evaluate(
+                """
+                var foo = "foo!";
+                java.util.Optional<String> v = {{package}}.Foo.newBuilder()
+                    .set{{Name}}(foo)
+                    .build()
+                    .get{{Name}}();
+                return foo == v.orElseThrow();
+                """,
+                Boolean.class,
+                "Name",
+                name));
+      }
     }
   }
 }
